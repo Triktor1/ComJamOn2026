7c83342ccb0ae5b8126238e9abc144ab40a2f458
Triktor <yosoyvictor06@gmail.com>
2026-02-13 16:51:16 +0100

    pequeñas correcciones

--- scripts/button_effects_component.gd
+++ scripts/button_effects_component.gd
@@ -3,7 +3,7 @@ class_name ButtonEffectsComponent
 
 @export var ease_type: Tween.EaseType
 @export var trans_type: Tween.TransitionType
-@export var duration: float
+@export var duration: float = 0.07
 @export var scale_amount: Vector2 = Vector2(1.1, 1.1)
 @export var rotation_amount: float = 3.0
 
@@ -21,7 +21,7 @@ func _on_mouse_hovered(hovered: bool) -> void:
 	tween.tween_property(button, "scale",
 		scale_amount if hovered else Vector2.ONE, duration)
 	tween.tween_property(button, "rotation_degrees",
-		rotation_amount * [-1, 1].pick_random() if hovered else 0.0, duration)
+		rotation_amount * [-1, 1].pick_random() if hovered else 0.0, duration).from(0)
 
 func reset_tween() -> void:
 	if tween:
