69699747462d6436ff1a85a2bea86a5ba0f14fac
AdrianDeMiguelCerezo <adriadem@ucm.es>
2026-02-17 16:55:02 +0100

    audio manager

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var active_music_stream:AudioStreamPlayer
+
+@export_group("Main")
+@export var clips:Node
+@export var one_shots:Node
+@export var audio_one_shot_scene:PackedScene
+
+func play(audio_name:String,from_position:float=0.0,skip_restart:bool=false)->void:
+	if skip_restart and active_music_stream and active_music_stream.name==audio_name:
+		return
+	active_music_stream=clips.get_node(audio_name)
+	active_music_stream.play(from_position)
+
+func play_one_shot(audio_stream:AudioStream,volume_db:float=0.0,from_position:float=0.0)->AudioOneShot:
+	var audio_one_shot:AudioOneShot=audio_one_shot_scene.instantiate()
+	audio_stream.stream=audio_stream
+	audio_one_shot.volume_db=volume_db
+	audio_one_shot.from_position=from_position
+	
+	one_shots.add_child(audio_one_shot)
+	return audio_one_shot
