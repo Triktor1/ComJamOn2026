9b8c5d0983625145fc5687c7e7d23c730b3bb4c0
AdrianDeMiguelCerezo <adriadem@ucm.es>
2026-02-20 13:28:15 +0100

    cambiado inputs

--- scripts/minigame_gamaner.gd
+++ scripts/minigame_gamaner.gd
@@ -1,14 +1,11 @@
 extends Node
 
 @export var isWining = true
-@export var time = 8.0 
+@export var time = 8.0
 
 var end = false
 
 var timer = 0.0
-
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
