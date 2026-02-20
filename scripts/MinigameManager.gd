d7ccfe9844256cf7b1fae36fd31221bfafdd185d
Ribir13 <victoa04@ucm.es>
2026-02-20 12:50:55 +0100

    MinigameManager

---
+++ scripts/MinigameManager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@export var isWining = true
+@export var time = 8.0 
+
+var end = false
+
+var timer = 0.0
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	timer += delta
+	
+	if timer >= time and !end:
+		end = true
+		if (isWining):
+			GameManager._state_win()
+		else:
+			GameManager._state_lose()
+			
