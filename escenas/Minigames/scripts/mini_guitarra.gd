48d5bf69107c931c8c77e155437b0dc30ea79cff
AdrianDeMiguelCerezo <adriadem@ucm.es>
2026-02-21 16:31:39 +0100

    mini guitarra sprites añadidos

--- escenas/Minigames/scripts/mini_guitarra.gd
+++ escenas/Minigames/scripts/mini_guitarra.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-@export var key1:Label
-@export var key2:Label
-@export var key3:Label
-@export var key4:Label
+@export var key1:Sprite2D
+@export var key2:Sprite2D
+@export var key3:Sprite2D
+@export var key4:Sprite2D
 @export var minigame_manager:MiniGameManager
 
-var keys:Array=[]
+@export var sprites:Array[Texture2D]
+var sprites_left:Array=[]
 var keys_to_press:Array=[]
 var current_key=0
 
@@ -16,7 +17,7 @@ var done:bool=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	keys=[key1,key2,key3,key4]
+	sprites_left=[key1,key2,key3,key4]
 	keys_to_press.push_back(randi_range(1,4))
 	keys_to_press.push_back(randi_range(1,4))
 	keys_to_press.push_back(randi_range(1,4))
@@ -28,31 +29,31 @@ func _ready() -> void:
 	assign_key(keys_to_press[2],key3)
 	assign_key(keys_to_press[3],key4)
 
-func assign_key(number:int,label:Label):
+func assign_key(number:int,sprite:Sprite2D):
 	if number==1:
-		label.text="W"
+		sprite.texture=sprites[0]
 	elif number==2:
-		label.text="A"
+		sprite.texture=sprites[1]
 	elif number==3:
-		label.text="S"
+		sprite.texture=sprites[2]
 	elif number==4:
-		label.text="D"
-	label.global_position.y=heights[number-1]
+		sprite.texture=sprites[3]
+	sprite.global_position.y=heights[number-1]
 
 func _input(event: InputEvent) -> void:
 	if not done:
 		if event is InputEventKey and event.pressed and not event.echo:
 			if event.is_action_pressed("Up") and keys_to_press[current_key]==1:
-				keys[current_key].text=""
+				sprites_left[current_key].texture=null
 				current_key+=1
 			elif event.is_action_pressed("Left") and keys_to_press[current_key]==2:
-				keys[current_key].text=""
+				sprites_left[current_key].texture=null
 				current_key+=1
 			elif event.is_action_pressed("Down") and keys_to_press[current_key]==3:
-				keys[current_key].text=""
+				sprites_left[current_key].texture=null
 				current_key+=1
 			elif event.is_action_pressed("Right") and keys_to_press[current_key]==4:
-				keys[current_key].text=""
+				sprites_left[current_key].texture=null
 				current_key+=1
 			else:
 				print("lose :(") #perder automaticamente
