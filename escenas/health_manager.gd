aed729981830305f72094e26cc72de5c857d3970
Ribir13 <victoa04@ucm.es>
2026-02-21 01:37:54 +0100

    health en transiciones

--- escenas/health_manager.gd
+++ escenas/health_manager.gd
@@ -1,11 +1,53 @@
 extends Node2D
 
+@export var heart_scene: PackedScene
+@export var start_lives: int = 4
 
-# Called when the node enters the scene tree for the first time.
+var hearts: Array[Node2D] = []
+var current_lives: int = 0
+
 func _ready():
-	pass # Replace with function body.
+	start_lives = GameManager.lives
+	
+	GameManager.lost.connect(_on_player_lost)
+	GameManager.comence.connect(_on_game_start)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_game_start():
+	_initialize_hearts(start_lives)
+
+func _initialize_hearts(lives: int):
+	_clear_hearts()
+	current_lives = lives
+	
+	for i in range(lives):
+		var heart = heart_scene.instantiate()
+		add_child(heart)
+		
+		heart.position.x = i * (heart.get_node("AnimatedSprite2D").sprite_frames.get_frame_texture("heart_idle", 0).get_width())
+		
+		var anim = heart.get_node("AnimatedSprite2D")
+		anim.play("heart_idle")
+		
+		hearts.append(heart)
+
+
+func _on_player_lost():
+	if current_lives <= 0:
+		return
+	
+	current_lives -= 1
+	
+	var heart = hearts[current_lives]
+	var anim = heart.get_node("AnimatedSprite2D")
+	anim.play("heart_death")
+	
+	await anim.animation_finished
+	heart.visible = false
+
+
+func _clear_hearts():
+	for h in hearts:
+		if is_instance_valid(h):
+			h.queue_free()
+	hearts.clear()
