b64bfe1c127e3b36dbf1ece5467b9a13d531000b
AdrianDeMiguelCerezo <adriadem@ucm.es>
2026-02-22 10:22:42 +0100

    end screen

---
+++ scripts/end_screen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@export var main_menu_button:Button
+@export var label_score:Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	main_menu_button.pressed.connect(_go_to_main_menu)
+	label_score.text=str(GameManager.get_mini_count())
+
+
+func _go_to_main_menu():
+	get_tree().change_scene_to_file("res://escenas/mainMenu.tscn")
